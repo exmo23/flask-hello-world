2365923c9ac5534803ff399bc5c80956798d9962
exmo23 <almo3346@colorado.edu>
2026-08-01 14:21:43 -0600

    database test

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import psycog2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World from Alex in 3308'
+
+
+@app.route("/db_test")
+def db_test():
+    conn = None
+    try:
+        DATABASE_URL = os.environ.get("DATABASE_URL")
+        conn = psycog2.connect(DATABASE_URL)
+
+    finally:
+        if conn is not None:
+            conn.close()
